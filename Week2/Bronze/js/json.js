75ee8e774bc387a294801a8ee223545157572bc7
brandonmcghee <brandonmcghee@fullsail.edu>
2012-08-09 23:07:41 -0400

    Week 2 Assignment

---
+++ Week2/Bronze/js/json.js
@@ -0,0 +1,180 @@
+//Brandon McGhee
+//Visual Frameworks Project 4
+
+var json = {
+    "spirit1": {
+        "spiritName":   ["Name: ", "Cabo Wabo"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Tequila"],
+        "date":         ["Date Purchased: ", "2012-07-17"]
+    },
+    "spirit2": {
+        "spiritName":   ["Name: ", "Jack Daniels"],
+        "bottleMIL":    ["Bottle Size: ", "1.5 L"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2012-02-14"]
+    },
+    "spirit3": {
+        "spiritName":   ["Name: ", "Malibu Coconut Rum"],
+        "bottleMIL":    ["Bottle Size: ", "375 ML"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Rum"],
+        "date":         ["Date Purchased: ", "2012-05-02"]
+    },
+    "spirit4": {
+        "spiritName":   ["Name: ", "99 Bananas"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Brandy"],
+        "date":         ["Date Purchased: ", "2010-01-17"]
+    },
+    "spirit5": {
+        "spiritName":   ["Name: ", "Narutotai"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Sake"],
+        "date":         ["Date Purchased: ", "2008-09-12"]
+    },
+    "spirit6": {
+        "spiritName":   ["Name: ", "Dolin"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Vermouth"],
+        "date":         ["Date Purchased: ", "2011-12-30"]
+    },
+    "spirit7": {
+        "spiritName":   ["Name: ", "Seagrems"],
+        "bottleMIL":    ["Bottle Size: ", "375 ML"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Gin"],
+        "date":         ["Date Purchased: ", "2012-02-14"]
+    },
+    "spirit8": {
+        "spiritName":   ["Name: ", "Evan Williams Cherry"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2012-06-10"]
+    },
+    "spirit9": {
+        "spiritName":   ["Name: ", "Patron Silver"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Tequila"],
+        "date":         ["Date Purchased: ", "2011-11-22"]
+    },
+    "spirit10": {
+        "spiritName":   ["Name: ", "Grey Goose"],
+        "bottleMIL":    ["Bottle Size: ", "1.5 L"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Vodka"],
+        "date":         ["Date Purchased: ", "2012-03-01"]
+    },
+    "spirit11": {
+        "spiritName":   ["Name: ", "Jameson 18 Years"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2012-03-17"]
+    },
+    "spirit12": {
+        "spiritName":   ["Name: ", "Aristocrat"],
+        "bottleMIL":    ["Bottle Size: ", "3 L"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Vodka"],
+        "date":         ["Date Purchased: ", "2009-10-25"]
+    },
+    "spirit13": {
+        "spiritName":   ["Name: ", "Makers Mark"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2010-06-29"]
+    },
+    "spirit14": {
+        "spiritName":   ["Name: ", "Beefeater"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Gin"],
+        "date":         ["Date Purchased: ", "2012-04-09"]
+    },
+    "spirit15": {
+        "spiritName":   ["Name: ", "Crystal Skull"],
+        "bottleMIL":    ["Bottle Size: ", "375 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Vodka"],
+        "date":         ["Date Purchased: ", "2012-07-12"]
+    },
+    "spirit16": {
+        "spiritName":   ["Name: ", "Baileys"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2012-06-06"]
+    },
+    "spirit17": {
+        "spiritName":   ["Name: ", "Aftershock"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Brandy"],
+        "date":         ["Date Purchased: ", "2006-01-03"]
+    },
+    "spirit18": {
+        "spiritName":   ["Name: ", "Balvenie 12 Year"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2009-09-25"]
+    },
+    "spirit19": {
+        "spiritName":   ["Name: ", "Hennessey"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Cognac"],
+        "date":         ["Date Purchased: ", "2012-05-02"]
+    },
+    "spirit20": {
+        "spiritName":   ["Name: ", "Bel Ange"],
+        "bottleMIL":    ["Bottle Size: ", "375 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Cognac"],
+        "date":         ["Date Purchased: ", "2012-07-12"]
+    },
+    "spirit21": {
+        "spiritName":   ["Name: ", "Bukito"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Sake"],
+        "date":         ["Date Purchased: ", "2012-04-19"]
+    },
+    "spirit22": {
+        "spiritName":   ["Name: ", "Aristocrat"],
+        "bottleMIL":    ["Bottle Size: ", "1.5 L"],
+        "shelve":       ["Quality: ", "Bottom Shelve"],
+        "family":       ["Family: ", "Tequila"],
+        "date":         ["Date Purchased: ", "2012-02-26"]
+    },
+    "spirit23": {
+        "spiritName":   ["Name: ", "Smirnoff Whipped"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Vodka"],
+        "date":         ["Date Purchased: ", "2012-07-04"]
+    },
+    "spirit24": {
+        "spiritName":   ["Name: ", "Gentleman Jack"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Top Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2011-11-18"]
+    },
+    "spirit25": {
+        "spiritName":   ["Name: ", "Johnnie Walker Red"],
+        "bottleMIL":    ["Bottle Size: ", "750 ML"],
+        "shelve":       ["Quality: ", "Middle Shelve"],
+        "family":       ["Family: ", "Whiskey"],
+        "date":         ["Date Purchased: ", "2010-01-02"]
+    }
+}
